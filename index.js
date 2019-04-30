f2b87c7ef6d8a18265d16af6b533c78a92321c34
MJmajiong <936233505@qq.com>
2019-04-30 19:34:13 +0800

    更新

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ app.get("/api/*", function (request, response, next){
 });
 
 
-app.get("/api/getAllStudent", loader.get("/api/getAllStudent"));
-app.get("/api/addStudent", loader.get("/api/addStudent"));
-app.get("/login", loader.get("/login"));
-app.post("/upload", uploadSingle.single("filename"), loader.get("/upload"));      //指定哪个字段是文件
-app.get('/getPic', loader.get("/getPic"));
+// app.get("/api/getAllStudent", loader.get("/api/getAllStudent"));
+// app.get("/api/addStudent", loader.get("/api/addStudent"));
+// app.get("/login", loader.get("/login"));
+// app.post("/upload", uploadSingle.single("filename"), loader.get("/upload"));      //指定哪个字段是文件
+// app.get('/getPic', loader.get("/getPic"));
+// app.post("/api/saveData", jsonParser, loader.get("/api/saveData"));
 
-app.post("/api/saveData", jsonParser, loader.get("/api/saveData"));
 app.post("/api/search", jsonParser, loader.get("/api/search"));
 app.get("/api/getClassInformation",loader.get("/api/getClassInformation"));
 
